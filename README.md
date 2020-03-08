43259ff52f43ebfd60034c6b7e51b900a5edfa01
Frankie Holzapfel <frankjh3@gmail.com>
2020-03-08 16:42:52 -0500

    Update README and add WebConfiguration handling

---
+++ README.md
@@ -0,0 +1,7 @@
+# Guinea Pig Locator
+Web app using Java Spring Boot and React. <br />
+Users are able to view guinea pigs available for adoption, submit listings containing
+guinea pigs available for adoption, and mark guinea pigs as adopted.
+## To build and run:
+Run `mvn clean install` from the home directory to build a jar containing the front and back end. 
+Then run `java -jar guinea-pig-locator-{VERSION}.jar` to start the tomcat server and react.
---
+++ src/main/java/com/locator/guineapiglocator/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.locator.guineapiglocator;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+//                .setViewName("forward:/");
+        registry.addViewController("/guinea_pig_pictures")
+                .setViewName("guinea_pig_pictures");
+    }
+}
